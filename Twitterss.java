a407324d571801b2139d95ff4c924ff5960ae1fc
Sorasak Khumsukha <sorasak.khumsukha@hotmail.com>
2018-04-23 06:07:15 +0700

    Add counting tweet and write it  to txt file
    -Add counting tweet.
    -write all tweet that we found to txt file

--- Twitterss.java
+++ Twitterss.java
@@ -1,3 +1,9 @@
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -8,14 +14,19 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class Twitterss {
 	
+	static BufferedWriter bw=null;
+    static FileWriter fw=null;
+	
 	static final String hashtag = "#BlackPanther";
-	static final int count = 100;
+	static final int count = 65536;
 	static long sinceId = 0;
 	static long numberOfTweets = 0;
-	
-	public static void main(String[] args) throws TwitterException {
+	static String collectTweets = "";
+	public static void main(String[] args) throws TwitterException, FileNotFoundException, IOException {
 
 		ConfigurationBuilder cb = new ConfigurationBuilder();
+        fw=new FileWriter("C:/Users/Pae/Desktop/Test.txt");
+        bw=new BufferedWriter(fw);
 		cb.setOAuthConsumerKey("FgZ2RfP7iXAt5n7ZicOqVPi7n")
 			.setOAuthConsumerSecret("6JL8DwmgKB9AGHJenBGrrer5HkcLtCTA3y9OjTaLuTjUYCGaQ8")
 			.setOAuthAccessToken("546477718-P2Y8SAOJirOv1Tpop5RTgsBnWH3LluzWA31v3hZO")
@@ -29,12 +40,11 @@ public class Twitterss {
 			System.out.println(st.getUser().getName()+"-------------------"+st.getText());
 		}*/
 		
-
 		Query queryMax = new Query(hashtag);
 		queryMax.setCount(count);
 		getTweets(queryMax, twitter, "maxId");
 		queryMax = null;
-		
+
 		do{
 			Query querySince = new Query(hashtag);
 			querySince.setCount(count);
@@ -42,6 +52,8 @@ public class Twitterss {
 			getTweets(querySince, twitter, "sinceId");
 			querySince = null;
 		}while(checkIfSinceTweetsAreAvailable(twitter));
+		
+		bw.close();
 	}
 	
 	private static boolean checkIfSinceTweetsAreAvailable(Twitter twitter){
@@ -77,28 +89,32 @@ public class Twitterss {
 				if(result.getTweets() == null || result.getTweets().isEmpty()){
 					getTweets = false;
 				}else{
-					System.out.println("***************************");
-					System.out.println("Gethered " + result.getTweets().size() + " tweets");
+					//System.out.println("***************************");
+					//System.out.println("Gethered " + result.getTweets().size() + " tweets");
+					long xx = 0;
 					int forCount = 0;
 					for(Status status: result.getTweets()){
+						xx++;
 						if(whileCount == 0 && forCount == 0){
 							sinceId = status.getId();
 							System.out.println("SinceID = "+sinceId);
-						}
+						}/*
 						System.out.println("Id = " + status.getId());
 						System.out.println("@" + status.getUser().getScreenName() 
 								+ " : " + status.getUser().getName()
-								+ "---" + status.getText());
+								+ "---" + status.getText());*/
 						if(forCount == result.getTweets().size()-1){
 							maxId = status.getId();
-							System.out.println("maxId = " + maxId);
+							//System.out.println("maxId = " + maxId);
 						}
 						System.out.println("");
+						collectTweets="\nCount: :" +xx+ ": >>>>>>>...ID: " + status.getId() + "::::::::" + status.getUser().getName() +"::::::::::"+ status.getText() + "::::Next::::\n";
+						bw.write(collectTweets);
 						forCount++;
 					}
 					numberOfTweets = numberOfTweets + result.getTweets().size();
 					query.setMaxId(maxId-1);
-					
+					System.out.println("Total tweets count ======= "+ numberOfTweets);
 				}
 			}catch (TwitterException te) {
                 System.out.println("Couldn't connect: " + te);
